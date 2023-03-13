b05b65fe02c06a266d395d7dca4235c9b5608f5b
SaoDiSeng <1300808192@qq.com>
2023-03-13 16:59:04 +0800

    2023年3月13日16:58:57

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -707,12 +707,47 @@ public class Solution {
          */
     }
 
+    /***
+     * 剑指 Offer 48. 最长不含重复字符的子字符串
+     * 请从字符串中找出一个最长的不包含重复字符的子字符串，计算该最长子字符串的长度。
+     * 示例 1:
+     * 输入: "abcabcbb"
+     * 输出: 3
+     * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+     * 示例 2:
+     * 输入: "bbbbb"
+     * 输出: 1
+     * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+     * 示例 3:
+     * 输入: "pwwkew"
+     * 输出: 3
+     * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     *      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+     *
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring(String s) {
+
+        Map<Character,Integer> dic = new HashMap<>();
+        int res = 0,temp = 0;
+        for (int j = 0;j < s.length();j++){
+            int i = dic.getOrDefault(s.charAt(j),-1);
+            dic.put(s.charAt(j),j);
+            temp = temp < j - i ? temp + 1 : j - i;
+            res = Math.max(res,temp);
+        }
+        return res;
+
+    }
+
+
 
 
     public static void main(String[] args) {
         int[][] a = new int[][]{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}};
         Solution solution = new Solution();
-        int repeatNumber = solution.translateNum(18822);
+        int repeatNumber = solution.lengthOfLongestSubstring("abcabcbb");
         System.out.println(repeatNumber);
     }
 
