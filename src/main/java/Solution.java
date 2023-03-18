b288b11389fefecd1f51fdb40e990ada9821604b
SaoDiSeng <1300808192@qq.com>
2023-03-18 21:04:17 +0800

    2023年3月18日21:04:10

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -918,6 +918,16 @@ public class Solution {
     }
 
     /***
+     * 剑指 Offer 57. 和为s的两个数字
+     * 输入一个递增排序的数组和一个数字s，在数组中查找两个数，使得它们的和正好是s。如果有多对数字的和等于s，则输出任意一对即可。
+     * 示例 1：
+     *
+     * 输入：nums = [2,7,11,15], target = 9
+     * 输出：[2,7] 或者 [7,2]
+     * 示例 2：
+     *
+     * 输入：nums = [10,26,30,31,47,60], target = 40
+     * 输出：[10,30] 或者 [30,10]
      *
      * @param nums
      * @param target
@@ -925,6 +935,66 @@ public class Solution {
      */
     public int[] twoSum(int[] nums, int target) {
 
+//        int[] res = new int[2];
+//        int target1 = 0;
+//        int target2 = 0;
+//        for (int i = 0; i < nums.length; i++) {
+//            target1 = nums[i];
+//            target2 = target - nums[i];
+//            for (int j = i + 1; j < nums.length; j++) {
+//                if (nums[j] == target2) {
+//                    res[0] = target1;
+//                    res[1] = target2;
+//                    return res;
+//                }
+//            }
+//        }
+//        return res;
+        int i = 0;
+        int j = nums.length - 1;
+        while(i < j){
+            int s = nums[i] + nums[j];
+            if (s < target) i++;
+            else if (s > target) j--;
+            else return new int[]{nums[i],nums[j]};
+        }
+        return new int[0];
+    }
+
+    /***
+     * @Description:
+     * 输入一个英文句子，翻转句子中单词的顺序，但单词内字符的顺序不变。为简单起见，标点符号和普通字母一样处理。例如输入字符串"I am a student. "，则输出"student. a am I"。
+     * 示例 1：
+     *
+     * 输入: "the sky is blue"
+     * 输出: "blue is sky the"
+     * 示例 2：
+     *
+     * 输入: "  hello world!  "
+     * 输出: "world! hello"
+     * 解释: 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+     * 示例 3：
+     *
+     * 输入: "a good   example"
+     * 输出: "example good a"
+     * 解释: 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+     * @param: [s]
+     * @return: java.lang.String
+     * @Author: SaoDiSeng
+     * @Date: 2023-03-76 21:40:57
+     */
+    public String reverseWords(String s) {
+        StringBuilder sb = new StringBuilder();
+        s = s.trim();
+        int i = s.length() - 1;
+        int j = i;
+        while(i >= 0){
+            while(i >= 0 && s.charAt(i) != ' ') i--;
+            sb.append(s.substring(i+1,j+1) + " ");
+            while(i >= 0 && s.charAt(i) == ' ') i--;
+            j = i;
+        }
+        return sb.toString().trim();
     }
 
 
@@ -933,6 +1003,9 @@ public class Solution {
         Solution solution = new Solution();
         int[] repeatNumber = solution.exchange(new int[]{1,2,3,4});
         System.out.println(Arrays.toString(repeatNumber));
+        String s = "    ni  ni ni ";
+        String s1 = solution.reverseWords(s);
+        System.out.println(s1);
     }
 
 }
