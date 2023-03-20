ef0c189100fb9b24f96c2fc936c02b14acc42ffb
SaoDiSeng <1300808192@qq.com>
2023-03-20 10:12:58 +0800

    2023年3月20日10:12:51

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -997,6 +997,45 @@ public class Solution {
         return sb.toString().trim();
     }
 
+    /***
+     * @Description: 剑指 Offer 12. 矩阵中的路径
+     * 给定一个m x n 二维字符网格board 和一个字符串单词word 。如果word 存在于网格中，返回 true ；否则，返回 false 。
+     * 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
+     * 示例 1：
+     * 输入：board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+     * 输出：true
+     * 示例 2：
+     * 输入：board = [["a","b"],["c","d"]], word = "abcd"
+     * 输出：false
+     * @param: [board, word]
+     * @return: boolean
+     * @Author: SaoDiSeng
+     * @Date: 2023-03-77 21:04:44
+     */
+    public boolean exist(char[][] board, String word) {
+        char[] words = word.toCharArray();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (dfs(board,words,i,j,0)) return true;
+            }
+        }
+        return false;
+    }
+    private boolean dfs(char[][] board, char[] words, int i, int j, int k) {
+        if(i >= board.length || i < 0 || j >= board[0].length || j < 0 || board[i][j] != words[k]) return false;
+        if(k == words.length - 1) return true;
+        board[i][j] = '\0';
+        boolean res = dfs(board,words,i+1,j,k+1) || dfs(board,words,i,j+1,k+1) ||
+                      dfs(board,words,i-1,j,k+1) || dfs(board,words,i,j-1,k+1);
+        board[i][j] = words[k];
+        return res;
+    }
+
+
+    public int movingCount(int m, int n, int k) {
+
+    }
+
 
     public static void main(String[] args) {
         int[][] a = new int[][]{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}};
