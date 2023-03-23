48ce5018a29dd736564813d4e55a8a61b3ef2ce5
SaoDiSeng <1300808192@qq.com>
2023-03-23 19:02:55 +0800

    2023年3月23日19:02:48

--- src/main/java/JianZhiOfferSolution.java
+++ src/main/java/JianZhiOfferSolution.java
@@ -1256,6 +1256,48 @@ public class JianZhiOfferSolution {
     }
 
 
+    /***
+     * @Description: 1626. 无矛盾的最佳球队
+     * 假设你是球队的经理。对于即将到来的锦标赛，你想组合一支总体得分最高的球队。球队的得分是球队中所有球员的分数 总和 。
+     * 然而，球队中的矛盾会限制球员的发挥，所以必须选出一支 没有矛盾 的球队。如果一名年龄较小球员的分数 严格大于 一名年龄较大的球员，则存在矛盾。同龄球员之间不会发生矛盾。
+     * 给你两个列表 scores 和 ages，其中每组 scores[i] 和 ages[i] 表示第 i 名球员的分数和年龄。请你返回 所有可能的无矛盾球队中得分最高那支的分数 。
+     * 输入：scores = [1,3,5,10,15], ages = [1,2,3,4,5]
+     * 输出：34
+     * 解释：你可以选中所有球员。
+     * 输入：scores = [4,5,6,5], ages = [2,1,2,1]
+     * 输出：16
+     * 解释：最佳的选择是后 3 名球员。注意，你可以选中多个同龄球员。
+     * @param: [scores, ages]
+     * @return: int
+     * @Author: SaoDiSeng
+     * @Date: 2023-03-81 11:27:11
+     */
+    public int bestTeamScore(int[] scores, int[] ages) {
+        int n = scores.length;
+        int[][] people = new int[n][2];
+        for (int i = 0; i < n; ++i) {
+            people[i] = new int[]{scores[i], ages[i]};
+        }
+        Arrays.sort(people, (a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
+        for (int i = 0; i < people.length; i++) {
+            System.out.print(people[i][0]);
+            System.out.println(people[i][1]);
+        }
+        int[] dp = new int[n];
+        int res = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i - 1; j >= 0; --j) {
+                if (people[j][1] <= people[i][1]) {
+                    dp[i] = Math.max(dp[i], dp[j]);
+                }
+            }
+            dp[i] += people[i][0];
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+
+
 
 
     public static void main(String[] args) {
@@ -1269,6 +1311,9 @@ public class JianZhiOfferSolution {
         System.out.println(count);
         String s2 = jianZhiOfferSolution.minNumber(new int[]{3,30,34,5,9,3});
         System.out.println(s2);
+        int i = jianZhiOfferSolution.bestTeamScore(new int[]{1, 3, 5, 10, 15}, new int[]{1, 2, 3, 4, 5});
+        Map<Integer,Integer> map = new HashMap<>();
+        map.getOrDefault(map.get(),0) +1
     }
 
 }
